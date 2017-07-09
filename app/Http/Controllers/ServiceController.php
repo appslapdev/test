b6268520cb0ce44907eb6552825c1b725c01e2d3
Admin <viktordbv@gmail.com>
2017-07-09 20:42:55 +0300

    added service controller

---
+++ app/Http/Controllers/ServiceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use App\User;
+use App\Http\Controllers\Controller;
+
+class ServiceController extends Controller
+{
+    public function login()
+    {
+        
+    }
+}
